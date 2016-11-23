da00b842786f619c17b4cbb1cda23ee77f6a8495
Brian Faust <faustbrian@users.noreply.github.com>
2016-11-23 16:15:41 +0200

    Applied fixes from StyleCI (#3)
    [ci skip] [skip ci]

--- resources/config/recaptcha.php
+++ resources/config/recaptcha.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of Laravel Recaptcha.
+ *
+ * (c) Brian Faust <hello@brianfaust.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 return [
 
     'site_key' => env('RECAPTCHA_SITE_KEY'),
